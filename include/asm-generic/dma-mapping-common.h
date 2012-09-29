b7932ed9f1964e2c8286d26374eeab9e372c54c3
Simon Sickle <simonsimons34@gmail.com>
2012-09-29 16:59:56 -0400

    testing

--- include/asm-generic/dma-mapping-common.h
+++ include/asm-generic/dma-mapping-common.h
@@ -43,7 +43,7 @@ static inline int dma_map_sg_attrs(struct device *dev, struct scatterlist *sg,
 				   int nents, enum dma_data_direction dir,
 				   struct dma_attrs *attrs)
 {
-	struct dma_map_ops *ops = get_dma_ops(dev);
+	//struct dma_map_ops *ops = get_dma_ops(dev);
 	int i, ents;
 	struct scatterlist *s;
 
